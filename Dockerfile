a10dc16d03aaa8d741d8a189cf3b8e98084188cb
Vrosten <528743+Vrosten@users.noreply.github.com>
2022-02-03 02:28:25 +0100

    Adjustment for Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM ubuntu: latest
+FROM python:latest
 
-WORKDIR /app/udp_tcp_totls
-COPY . /app/udp_tcp_totls/
-RUN apt-get -y update && apt-get install -y python3
-CMD ["python3","main.py"]
+WORKDIR /app/dns_proxy
+COPY . /app/dns_proxy
+RUN ["python3","main.py -i 192.168.0.21 -p 53"]
--- main.py
+++ main.py
@@ -1,27 +1,51 @@
-import socket
-import socketserver
-import threading
-
-
+import socket, socketserver, threading
+import sys, getopt
+import docker
 from lib.proxy_service import ThreadedDnsUdpHandler,ThreadedDnsTcpHandler,ThreadedTcpDnsProxy,ThreadedUdpDnsProxy
 
 #Asign the ip address of the container to bind the DNS port
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 9999
-print ("{} {}".format(HOST,PORT))
-#HOST, PORT = "192.168.0.13", 9999
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"i:p:",["interface=,port="])
+    except getopt.GetoptError:
+        print (">>>> Usage: main.py -i <ip_address> -p <port>")
+        sys.exit(2)
 
-socketserver.TCPServer.allow_reuse_address = True
-socketserver.UDPServer.allow_reuse_address = True
+    for opt,arg in opts:
+        if opt in ("-i","--interface"):
+            HOST = arg
+        elif opt in ("-p","--port"):
+            PORT = arg
 
-tcp_server = ThreadedTcpDnsProxy((HOST,PORT),ThreadedDnsTcpHandler)
-tcp_server_threaded = threading.Thread(target=tcp_server.serve_forever,daemon=True)
-tcp_server_threaded.start()
+    if HOST:
+        if PORT:
+            print("{} {}".format(HOST,PORT))
+            #HOST, PORT = "192.168.0.13", 9999
 
-udp_server = ThreadedUdpDnsProxy((HOST,PORT),ThreadedDnsUdpHandler)
-udp_server_threaded = threading.Thread(target=udp_server.serve_forever,daemon=True)
-udp_server_threaded.start()
+            socketserver.TCPServer.allow_reuse_address = True
+            socketserver.UDPServer.allow_reuse_address = True
 
-tcp_server_threaded.join()
-udp_server_threaded.join()
+            print(">>>> Initiating DNS proxy TCP based in {}:{} <<<<".format(HOST,PORT))
+            tcp_server = ThreadedTcpDnsProxy((HOST,PORT),ThreadedDnsTcpHandler)
+            tcp_server_threaded = threading.Thread(target=tcp_server.serve_forever,daemon=True)
+            tcp_server_threaded.start()
+            print(">>>> Listen on TCP {}:{} <<<<".format(HOST,PORT))
+
+            print(">>>> Initiating DNS proxy UDP based in {}:{} <<<<".format(HOST,PORT))
+            udp_server = ThreadedUdpDnsProxy((HOST,PORT),ThreadedDnsUdpHandler)
+            udp_server_threaded = threading.Thread(target=udp_server.serve_forever,daemon=True)
+            udp_server_threaded.start()
+            print(">>>> Listen on UDP {}:{} <<<<".format(HOST,PORT))
+
+            tcp_server_threaded.join()
+            udp_server_threaded.join()
+
+if __name__ == "__main__":
+    print("Initiating the proxy with {}".format(sys.argv[1:]))
+  
+    client = docker.DockerClient()
+    container = client.containers.get("magical_meitner")
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    print(ip_add)
+    #main(sys.argv[1:])
